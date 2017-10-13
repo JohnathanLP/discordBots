42cd3f4986f2bfdaddbf1228d3ba5815744544cb
JohnathanLP <johnathanlpowell@gmail.com>
2017-10-13 23:18:15 +0000

    added sounds, started work on auto-restart

---
+++ eyspbot/startbot.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+case "$(pidof python3 | wc -w)" in
+0)
+     echo "Restarting bot: $(date)" >> ~/discordBots/eyspbot/log.txt
+     tmux send -t 0.0 sudo\ python3\ /home/pi/discordBots/eyspbot/bot.py ENTER
+     ;;
+1)
+     echo "Bot is already running: $(date)" >> ~/discordBots/eyspbot/log.txt
+     ;;
+*)
+esac
+     
